a0a9e251f1c40f2cf697fc9d1e34e5f8a96ed557
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-06-26 21:12:55 +0200

    Remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
-import Link from "next/link";
+// import Link from "next/link";
 
 export default function Home() {
     return (
