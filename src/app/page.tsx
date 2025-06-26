883edabb35d535cfd9f94bc4fbde525fd3682528
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-06-26 20:20:00 +0200

    Add configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,37 @@
+import { Button } from "@/components/ui/button";
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
 } from "@/components/ui/dialog";
 
 export default function Home() {
-	return (
-		<main className="w-full h-full">
-			<Dialog>
-				<DialogTrigger id="dialog">Open</DialogTrigger>
-				<DialogContent>
-					<DialogHeader>
-						<DialogTitle>Are you absolutely sure?</DialogTitle>
-						<DialogDescription>
-							This action cannot be undone. This will permanently delete your
-							account and remove your data from our servers.
-						</DialogDescription>
-					</DialogHeader>
-				</DialogContent>
-			</Dialog>
-		</main>
-	);
+    return (
+        <main className="flex h-full w-full items-center justify-center">
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button
+                        type="button"
+                        id="dialog"
+                        className="cursor-pointer"
+                    >
+                        Open
+                    </Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Are you absolutely sure?</DialogTitle>
+                        <DialogDescription>
+                            This action cannot be undone. This will permanently
+                            delete your account and remove your data from our
+                            servers.
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+        </main>
+    );
 }
